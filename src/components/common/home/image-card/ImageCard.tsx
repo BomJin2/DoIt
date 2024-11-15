6769158f0cb0672b829b349962cdf262118607d8
Bomjin2 <opjkl4006@naver.com>
2024-11-15 16:31:34 +0900

    dialog ui 구현

--- src/components/common/home/image-card/ImageCard.tsx
+++ src/components/common/home/image-card/ImageCard.tsx
@@ -1,17 +1,17 @@
 import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
   Button,
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-  Input,
-  Label,
   Skeleton,
 } from "@/components/ui";
-import { FolderOpen, Heart } from "lucide-react";
+import { AlignLeft, ClipboardPenLine, FolderOpen, Heart, Pin } from "lucide-react";
 
 const ImageCard = () => {
   return (
@@ -24,28 +24,47 @@ const ImageCard = () => {
                 <FolderOpen className="h-5 w-5" />
               </Button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
+            <DialogContent className="sm:max-w-xl">
               <DialogHeader>
-                <DialogTitle>Edit profile</DialogTitle>
-                <DialogDescription>Make changes to your profile here. Click save when you're done.</DialogDescription>
+                <DialogTitle>이미지 상세 정보</DialogTitle>
+                <DialogDescription>선택한 이미지의 상세정보를 확인하세요.</DialogDescription>
               </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="name" className="text-right">
-                    Name
-                  </Label>
-                  <Input id="name" defaultValue="Pedro Duarte" className="col-span-3" />
+              <div className="flex flex-col items-center space-y-2 gap-3">
+                <Skeleton className="h-80 w-full rounded-xl" />
+                <div className="flex items-center justify-start w-full">
+                  <div className="flex items-center gap-2">
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <small className="text-sm font-medium leading-none">스나이퍼팩토리</small>
+                  </div>
                 </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="username" className="text-right">
-                    Username
-                  </Label>
-                  <Input id="username" defaultValue="@peduarte" className="col-span-3" />
+                <div className="flex flex-col w-full gap-2">
+                  <div className="flex items-center ">
+                    <Pin className="h-4 w-4 mt-[2px] mr-1" />
+                    <span className="text-sm">등대-근처의 한인 주택</span>
+                  </div>
+                  <div className="flex items-center ">
+                    <Pin className="h-4 w-4 mt-[2px] mr-1" />
+                    <span className="text-sm">등대-근처의 한인 주택</span>
+                  </div>
+                  <div className="flex items-center ">
+                    <Pin className="h-4 w-4 mt-[2px] mr-1" />
+                    <span className="text-sm">등대-근처의 한인 주택</span>
+                  </div>
+                </div>
+                <div className="flex items-center justify-end w-full gap-4">
+                  <div className="flex items-center gap-1 text-sm">
+                    <p className="leading-7">게시일:</p>
+                    2024-11-15
+                  </div>
+                  <div className="flex items-center gap-1 text-sm">
+                    <Heart className="h-[14px] w-[14px] mt-[2px] text-rose-600" fill="#e11d48" />
+                    1,000
+                  </div>
                 </div>
               </div>
-              <DialogFooter>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
             </DialogContent>
           </Dialog>
           {/* Skeleton UI: 이미지 데이터가 렌더링 되기 전 */}
@@ -59,7 +78,7 @@ const ImageCard = () => {
             {/* 게시일 */}
             <div className="flex items-center gap-1 text-sm">
               <span className="leading-7">게시일:</span>
-              <span className="leading-7">2024-11-15</span>
+              2024-11-15
             </div>
             {/* 좋아요 수 */}
             <div className="flex items-center gap-1 text-sm">
